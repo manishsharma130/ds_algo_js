e22eb248dcf9b36b38955355bfd7c48ccc9f39e7
Manish Sharma <letmedoanything@gmail.com>
2021-09-12 18:07:47 +0530

    best implementation deletion in bst

--- tree/treeDeletion.js
+++ tree/treeDeletion.js
@@ -59,19 +59,6 @@ BST.preOrderIterative = (bst) => {
 	console.log(str);
 };
 
-BST.minDeleteIterativeV1 = (bst) => {
-	let root = bst.root.right;
-	let prev = bst.root;
-	while (root.left !== null) {
-		prev = root;
-		root = root.left;
-	}
-	if (prev !== root) {
-		prev.right = null;
-	}
-	return root.data || null;
-};
-
 BST.minValueIterativeV1 = (bst) => {
 	let root = bst.root;
 	while (root.left !== null) root = root.left;
@@ -87,66 +74,106 @@ BST.maxValueIterativeV1 = (bst) => {
 // Deletion in binary search tree (BST)
 
 BST.deletionItrativeV1 = (bst, data = 0) => {
+	/**
+	 * Basic Steps to perform deletion algo.
+	 * step:-1 first need to find required data
+	 * step:-2 if data has not been found then we can simply stop the procedure
+	 * step:-3 if data data has been found then we can perform following steps
+	 *      1. we check if node has single and no child
+	 *      2. we check if node has two child
+	 */
+
 	let root = bst.root;
 	let prev = null;
-	let iterator = [];
-	let maxValue;
-	iterator.unshift(root);
-	while (root !== null || iterator.length !== 0) {
-		root = iterator.shift() || null;
-		// console.log(root !== null && root.data, data);
-		if (root !== null && root.data === data) {
-			// Perform deletion action
+	let tempNode, p;
 
-			if (root.left === null) {
-				console.log("left null");
-				console.log(prev, root);
-				if (prev !== null) {
-					if (prev.left === root) {
-						prev.left = root.left;
-					}
-					if (prev.right === root) {
-						prev.right = root.right;
-					}
-				} else bst.root = null;
-				return;
-			}
-			if (root.right === null) {
-				console.log("right null");
-				if (prev !== null) {
-					if (prev.left === root) {
-						prev.left = root.left;
-					}
-					if (prev.right === root) {
-						prev.right = root.right;
-					}
-				} else {
-					bst.root = null;
-				}
-				return;
-			}
-
-			// maxValue = BST.minValueIterativeV1({ root: root.right });
-			maxValue = BST.minDeleteIterativeV1({ root: root });
-			console.log(maxValue);
-			root.data = maxValue;
-			return;
-		} else {
-			if (root !== null) {
-				if (root.right !== null) iterator.unshift(root.right);
-				if (root.left !== null) iterator.unshift(root.left);
-
-				// Need to check parent
-				// if (prev === null) {
-				// 	prev = root;
-				// } else if (prev.left !== root && prev.right === root) {
-				// 	prev = root;
-				// } else if (prev.left === root && prev.right !== root) {
-				// 	// prev = root;
-				// }
-			}
-		}
+	/**
+	 * * Step:-1
+	 */
+	while (root !== null && root.data !== data) {
+		prev = root;
+		if (data < root.data) root = root.left;
+		else root = root.right;
 	}
+	/**
+	 * * Step:-2
+	 */
+	if (root === null) {
+		console.log(`Key ${data} not found in the provided BST`);
+		return;
+	}
+	/**
+	 * * Step:-3
+	 */
+	if (root.left === null || root.right === null) {
+		/**
+		 * * Step:-3
+		 * *   1.
+		 */
+
+		if (root.left === null) tempNode = root.right;
+		else tempNode = root.left;
+
+		if (prev === null) {
+			this.root = tempNode;
+			return;
+		}
+
+		if (prev.left === root) {
+			prev.left = tempNode;
+		} else {
+			prev.right = tempNode;
+		}
+	} else {
+		/**
+		 * * Step:-3
+		 * *   2.
+		 */
+		p = null;
+		tempNode = root.right;
+		while (tempNode.left !== null) {
+			p = tempNode;
+			tempNode = tempNode.left;
+		}
+
+		if (p !== null) p.left = tempNode.right;
+		else root.right = tempNode.right;
+		root.data = tempNode.data;
+	}
+};
+
+BST.deletionRecursiveV1 = (bst, data) => {
+	const deletionRec = (node, value) => {
+		/**
+		 ** Base Case: If the tree is empty
+		 */
+		// console.log("Data:- ", data);
+		if (node === null) return node;
+		/**
+		 ** Going down the tree recursively
+		 */
+		if (value < node.data) {
+			node.left = deletionRec(node.left, value);
+		} else if (value > node.data) {
+			node.right = deletionRec(node.right, value);
+		} else {
+			// node with only one child or no child
+			if (node.left === null) return node.right;
+			else if (node.right === null) return node.left;
+
+			//finding the minimum from right subtree
+			node.data = BST.minValueIterativeV1({ root: node.right });
+
+			// console.log(node.data);
+			//delete the inorder successor (That's mean minimum node that we found in right subtree)
+			node.right = deletionRec(node.right, node.data);
+		}
+		return node;
+	};
+
+	bst.root = deletionRec(bst.root, data);
+
+	return bst;
 };
 
 const bst = new BST();
@@ -159,7 +186,8 @@ BST.insertIterativeV1(bst, 50);
 BST.insertIterativeV1(bst, 6);
 
 BST.preOrderIterative(bst);
-BST.deletionItrativeV1(bst, 6);
+// BST.deletionItrativeV1(bst, 40);
+BST.deletionRecursiveV1(bst, 40);
 BST.preOrderIterative(bst);
 // console.log(BST.minValueIterativeV1(bst));
 // console.log(BST.maxValueIterativeV1(bst));
